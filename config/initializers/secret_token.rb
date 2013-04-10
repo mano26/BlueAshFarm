ccc45d6c211c4491eb106c773ca792efa483f53a
michael romano <mikeromano@gmail.com>
2013-04-10 18:48:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueAshFarm::Application.config.secret_token = 'fccb2d42c221ca42fd313db33b4e244560fe1e2bd607efaffbbb652269286a2eb1ea515ddd1d0048b54597cbe695c3f73e775d3cff258d54ed920aa801475c1a'
